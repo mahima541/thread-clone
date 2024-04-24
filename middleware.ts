63facd2856d550fdccdaa38b5f07e6edcb865ff7
mahima <mahimabsr8368@gmail.com>
2024-04-24 16:45:57 +0530

    commit

---
+++ middleware.ts
@@ -0,0 +1,30 @@
+// import {  authMiddleware }  from "@clerk/nextjs";
+// // import { clerkMiddleware } from "@clerk/nextjs";
+
+
+
+// export default authMiddleware({
+//   // An array of public routes that don't require authentication.
+//   publicRoutes: ["/api/webhook/clerk"],
+
+//   // An array of routes to be ignored by the authentication middleware.
+//   ignoredRoutes: ["/api/webhook/clerk"],
+// });
+
+// export const config = {
+//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware({
+    // An array of public routes that don't require authentication.
+  publicRoutes: ["/api/webhook/clerk"],
+
+  // An array of routes to be ignored by the authentication middleware.
+  ignoredRoutes: ["/api/webhook/clerk"],
+});
+
+export const config = {
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
